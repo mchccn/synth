bbc26651c365177e28ef2f053bc7c178b03399f1
kelsny <cursors.owo.uwu@gmail.com>
2022-06-10 13:03:34 -0500

    lc stuff

--- scripts/linecount.ts
+++ scripts/linecount.ts
@@ -1,45 +1,61 @@
 import chalk from "chalk";
-import { readdir, readFile, stat } from "fs/promises";
+import { constants } from "fs";
+import { access, readdir, readFile, stat } from "fs/promises";
 import { join } from "path";
 
-async function linecount(target: string, ext = "ts"): Promise<[count: number, sloc: number]> {
-    const stats = await stat(target);
+if (process.platform === "win32") throw new Error("Fuck off, Windows user.");
 
-    let count = 0;
+async function linecount(target: string, extensions = ["ts"]): Promise<[lines: number, sloc: number]> {
+    let lines = 0;
     let sloc = 0;
+
+    if (
+        await access(target, constants.R_OK)
+            .then(() => false)
+            .catch(() => true)
+    ) {
+        console.log(chalk.redBright`error -`, `cannot count ${target.replace(process.env.HOME!, "~")}`);
+
+        return [lines, sloc];
+    }
+
+    console.log(chalk.blueBright`info  -`, `now counting ${target.replace(process.env.HOME!, "~")}`);
+
+    const stats = await stat(target);
 
     if (stats.isDirectory()) {
         const dirents = await readdir(target, { withFileTypes: true });
 
-        await Promise.all(
-            dirents.map(async (d) => {
-                if (d.isFile() && d.name.endsWith("." + ext)) {
-                    const contents = await readFile(join(target, d.name), "utf8");
+        for (const d of dirents) {
+            if (d.isFile() && extensions.some((ext) => d.name.endsWith("." + ext))) {
+                console.log(
+                    chalk.magentaBright`event -`,
+                    `reading file ${join(target, d.name).replace(process.env.HOME!, "~")}`,
+                );
 
-                    count += contents.split("\n").length;
-                    sloc += contents.split("\n").filter((l) => l.trim()).length;
+                const contents = await readFile(join(target, d.name), "utf8");
 
-                    return;
-                }
+                lines += contents.split("\n").length;
+                sloc += contents.split("\n").filter((l) => l.trim()).length;
+            }
 
-                if (d.isDirectory()) {
-                    [count, sloc] = (await linecount(join(target, d.name), ext)).map((x, i) => x + [count, sloc][i]);
+            if (d.isDirectory()) {
+                [lines, sloc] = (await linecount(join(target, d.name), extensions)).map((x, i) => x + [lines, sloc][i]);
+            }
+        }
 
-                    return;
-                }
-            }),
-        );
-
-        return [count, sloc];
+        return [lines, sloc];
     }
 
     if (stats.isFile()) {
+        console.log(chalk.magentaBright`event -`, `reading file ${target.replace(process.env.HOME!, "~")}`);
+
         const contents = await readFile(target, "utf8");
 
-        count += contents.split("\n").length;
+        lines += contents.split("\n").length;
         sloc += contents.split("\n").filter((l) => l.trim()).length;
 
-        return [count, sloc];
+        return [lines, sloc];
     }
 
     return [NaN, NaN];
@@ -47,9 +63,9 @@ async function linecount(target: string, ext = "ts"): Promise<[count: number, sl
 
 const given = (process.argv[2] ?? process.env.LC_PATH ?? ".").split(/(?<!\\),/);
 const targets = given.map((path) => join(process.cwd(), path));
-const extension = process.argv[3] ?? "ts";
+const extensions = (process.argv[3] ?? process.env.LC_EXT ?? "ts").split(/(?<!\\),/);
 
-const stats = await Promise.all(targets.map((d) => linecount(d, extension)));
+const stats = await Promise.all(targets.map((d) => linecount(d, extensions)));
 
 const min = Math.min(...stats.flat());
 const max = Math.max(...stats.flat());
@@ -67,14 +83,14 @@ const grade = (count: number) =>
         : count;
 
 console.log(
-    chalk.bold(`\nLine count results\n`),
+    chalk.bold(`\nline count results\n`),
     stats
         .sort((a, b) => a[0] - b[0])
         .map(
             ([lines, sloc], i) => `
-${chalk.dim(given[i])}
-Total lines  ${chalk.gray(":")} ${grade(lines)}
-Source lines ${chalk.gray(":")} ${grade(sloc)}
+${chalk.dim(targets[i].replace(process.env.HOME!, "~"))}
+total lines  ${chalk.gray(":")} ${grade(lines)}
+source lines ${chalk.gray(":")} ${grade(sloc)}
 `,
         )
         .join(""),
