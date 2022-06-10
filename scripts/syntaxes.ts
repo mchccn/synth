39615777aa8ccfb364da88549146205c29b152b4
kelsny <cursors.owo.uwu@gmail.com>
2022-06-09 22:01:07 -0500

    extension stuff

---
+++ scripts/syntaxes.ts
@@ -0,0 +1,158 @@
+import { writeFileSync } from "fs";
+import { join, sep } from "path";
+import * as generators from "../dist/core/generators/index.js";
+
+const tmLanguage = {
+    $schema: "https://raw.githubusercontent.com/martinring/tmlanguage/master/tmlanguage.json",
+    name: "Synth",
+    patterns: [
+        { include: "#punctuation" },
+        { include: "#comments" },
+        { include: "#keywords" },
+        { include: "#strings" },
+        { include: "#property" },
+        { include: "#constants" },
+    ],
+    repository: {
+        punctuation: {
+            patterns: [
+                {
+                    name: "punctuation.definition.block.synth",
+                    match: "[\\{\\}\\[\\]]",
+                },
+                {
+                    name: "meta.brace.synth",
+                    match: "[\\(\\)]",
+                },
+            ],
+        },
+        comments: {
+            patterns: [
+                {
+                    name: "comment.single-line.synth",
+                    begin: "#",
+                    end: "\n",
+                },
+            ],
+        },
+        keywords: {
+            patterns: [
+                {
+                    name: "function.synth",
+                    match: `\\b(${Object.keys(generators).join("|")})\\b`,
+                },
+            ],
+        },
+        property: {
+            patterns: [
+                {
+                    name: "variable.synth",
+                    match: "[a-zA-Z_$][a-zA-Z0-9_$]*",
+                },
+            ],
+        },
+        strings: {
+            patterns: [
+                {
+                    name: "string.source.double.quo",
+                    begin: 's"',
+                    end: '"',
+                    patterns: [
+                        {
+                            name: "constant.character.escape.synth",
+                            match: "\\\\.",
+                        },
+                    ],
+                },
+                {
+                    name: "string.source.single.quo",
+                    begin: "s'",
+                    end: "'",
+                    patterns: [
+                        {
+                            name: "constant.character.escape.synth",
+                            match: "\\\\.",
+                        },
+                    ],
+                },
+                {
+                    name: "string.regexp.double.quo",
+                    begin: 'r"',
+                    end: '"',
+                    patterns: [
+                        {
+                            name: "constant.character.escape.synth",
+                            match: "\\\\.",
+                        },
+                    ],
+                },
+                {
+                    name: "string.regexp.single.quo",
+                    begin: "r'",
+                    end: "'",
+                    patterns: [
+                        {
+                            name: "constant.character.escape.synth",
+                            match: "\\\\.",
+                        },
+                    ],
+                },
+                {
+                    name: "string.quoted.double.quo",
+                    begin: '"',
+                    end: '"',
+                    patterns: [
+                        {
+                            name: "constant.character.escape.synth",
+                            match: "\\\\.",
+                        },
+                    ],
+                },
+                {
+                    name: "string.quoted.single.quo",
+                    begin: "'",
+                    end: "'",
+                    patterns: [
+                        {
+                            name: "constant.character.escape.synth",
+                            match: "\\\\.",
+                        },
+                    ],
+                },
+            ],
+        },
+        constants: {
+            patterns: [
+                {
+                    name: "constant.numeric.binary.synth",
+                    match: "0b[01_]+(\\.[01_]*)?(e[01_]+(\\.[01_]*)?)?",
+                },
+                {
+                    name: "constant.numeric.octal.synth",
+                    match: "0o[0-7_]+(\\.[0-7_]*)?(e[0-7_]+(\\.[0-7_]*)?)?",
+                },
+                {
+                    name: "constant.numeric.hex.synth",
+                    match: "0x[0-9a-fA-F_]+(\\.[0-9a-fA-F_]*)?(e[0-9a-fA-F_]+(\\.[0-9a-fA-F_]*)?)?",
+                },
+                {
+                    name: "constant.numeric.decimal.synth",
+                    match: "[\\d_]+(\\.[\\d_]*)?(e[\\d_]+(\\.[\\d_]*)?)?",
+                },
+                {
+                    name: "support.variable.synth",
+                    match: "\\b(true|false)\\b",
+                },
+            ],
+        },
+    },
+    scopeName: "source.synth",
+};
+
+const folder = new URL(import.meta.url).pathname.replaceAll("/", sep).replace(/\/([^/]+)$/, "");
+
+writeFileSync(
+    join(folder, "..", "extension", "syntaxes", "synth.tmLanguage.json"),
+    JSON.stringify(tmLanguage, undefined, 4),
+    "utf8",
+);
--- src/core/generators/index.ts
+++ src/core/generators/index.ts
@@ -1,16 +1,13 @@
 // cd dist/core/generators && for f in $(ls | grep -v index.js | grep -v generators.js | grep .js); do echo "export * from \"./$f\";"; done && cd ../../..
-export * from "./bigint";
 export * from "./bigint.js";
 export * from "./boolean.js";
 export * from "./function.js";
 export * from "./max.js";
 export * from "./min.js";
-export * from "./null";
 export * from "./null.js";
 export * from "./number.js";
 export * from "./range.js";
 export * from "./regex.js";
-export * from "./string";
 export * from "./string.js";
 export * from "./symbol.js";
 export * from "./undefined.js";
