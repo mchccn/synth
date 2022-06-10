99c2618d504e4b6c04903043800093e396b79e99
kelsny <cursors.owo.uwu@gmail.com>
2022-06-09 23:27:29 -0500

    sexier linecount script

--- scripts/linecount.ts
+++ scripts/linecount.ts
@@ -1,39 +1,82 @@
-import { readdir, readFile } from "fs/promises";
+import chalk from "chalk";
+import { readdir, readFile, stat } from "fs/promises";
 import { join } from "path";
 
-async function linecount(directory: string) {
-    const dirents = await readdir(directory, { withFileTypes: true });
+async function linecount(target: string, ext = "ts"): Promise<[count: number, sloc: number]> {
+    const stats = await stat(target);
 
     let count = 0;
     let sloc = 0;
 
-    await Promise.all(
-        dirents.map(async (d) => {
-            if (d.isFile() && d.name.endsWith(".ts")) {
-                const contents = await readFile(join(directory, d.name), "utf8");
+    if (stats.isDirectory()) {
+        const dirents = await readdir(target, { withFileTypes: true });
 
-                count += contents.split("\n").length;
-                sloc += contents.split("\n").filter((l) => l.trim()).length;
+        await Promise.all(
+            dirents.map(async (d) => {
+                if (d.isFile() && d.name.endsWith("." + ext)) {
+                    const contents = await readFile(join(target, d.name), "utf8");
 
-                return;
-            }
+                    count += contents.split("\n").length;
+                    sloc += contents.split("\n").filter((l) => l.trim()).length;
 
-            if (d.isDirectory()) {
-                [count, sloc] = (await linecount(join(directory, d.name))).map((x, i) => x + [count, sloc][i]);
+                    return;
+                }
 
-                return;
-            }
-        }),
-    );
+                if (d.isDirectory()) {
+                    [count, sloc] = (await linecount(join(target, d.name), ext)).map((x, i) => x + [count, sloc][i]);
 
-    return [count, sloc] as [count: number, sloc: number];
+                    return;
+                }
+            }),
+        );
+
+        return [count, sloc];
+    }
+
+    if (stats.isFile()) {
+        const contents = await readFile(target, "utf8");
+
+        count += contents.split("\n").length;
+        sloc += contents.split("\n").filter((l) => l.trim()).length;
+
+        return [count, sloc];
+    }
+
+    return [NaN, NaN];
 }
 
-const directory = join(process.cwd(), process.argv[2] ?? process.env.LC_PATH ?? ".");
+const targets = (process.argv[2] ?? process.env.LC_PATH ?? ".")
+    .split(/(?<!\\),/)
+    .map((path) => join(process.cwd(), path));
+const extension = process.argv[3] ?? "ts";
 
-const [lines, sloc] = await linecount(directory);
+const stats = await Promise.all(targets.map((d) => linecount(d, extension)));
 
-console.log(`
-Total lines: ${lines}
-Source lines: ${sloc}
-`);
+const min = Math.min(...stats.flat());
+const max = Math.max(...stats.flat());
+
+const scale = (value: number, [a1, a2]: [number, number], [b1, b2]: [number, number]) =>
+    ((value - a1) * (b2 - b1)) / (a2 - a1) + b1;
+
+const grade = (count: number) =>
+    chalk.level === 3
+        ? chalk.rgb(
+              Math.round(255 - scale(count, [min, max], [0, 1]) * 255),
+              Math.round(128 + scale(count, [min, max], [0, 1]) * 127),
+              Math.round(128 + scale(count, [min, max], [0, 1]) * 127),
+          )(count)
+        : count;
+
+console.log(
+    chalk.bold(`\nLine count results\n`),
+    stats
+        .sort((a, b) => a[0] - b[0])
+        .map(
+            ([lines, sloc], i) => `
+${chalk.dim(targets[i])}
+Total lines  ${chalk.gray(":")} ${grade(lines)}
+Source lines ${chalk.gray(":")} ${grade(sloc)}
+`,
+        )
+        .join(""),
+);
